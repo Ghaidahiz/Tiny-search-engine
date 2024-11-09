91b223fa26be52604d70ef930030bab173fe1038
emanameen9982773 <emanothard15@gmail.com>
2024-11-09 21:21:35 +0300

    method createIndexList was implemented + method removeStopWord is no longer needed

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,112 @@
 package src;
 
 import java.io.*;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        String[] line =createIndexList();
-        for (String value : line)
-        System.out.println(value+"\n");
+
+        LinkedList2<Integer, String> index = createIndexList();
+
+        // index.getList().findfirst();
+        // System.out.print(index.getList().retrieve()=="" );
+
+        // the folowing code is just to print our index lst
+        if (!index.empty()) {
+            index.findfirst();
+            while (!index.last()) {
+                System.out.print("\n\n" + index.retrieve() + ": ");
+                index.getList().findfirst();
+                if (!index.getList().empty()) {
+                    while (!index.getList().last()) {
+                        System.out.print(index.getList().retrieve() + "|");
+                        index.getList().findnext();
+                    }
+                    System.out.print(index.getList().retrieve() + "|");
+                }
+                index.findnext();
+            }
+            System.out.print("\n\n" + index.retrieve() + ": ");
+            index.getList().findfirst();
+            while (!index.getList().last()) {
+                System.out.print(index.getList().retrieve() + ", ");
+                index.getList().findnext();
+            }
+            System.out.print(index.getList().retrieve() + ", ");
+        }
+        // for (String value : line)
+        // System.out.println(value+"\n");
+
     }
 
-    //public static LinkedList2<Integer,String > createIndexList() throws Exception{
-        public static String[] createIndexList() throws Exception{
-        File dataset = new File("C:\\Users\\EMAN\\Desktop\\data-structure-project-2\\dataset.csv");
-        LinkedList2<Integer,String> indextList = new LinkedList2<Integer,String>();
-        BufferedReader br = new BufferedReader(new FileReader(dataset));
-        String line = br.readLine();
-         line = br.readLine();
-        
-        //while ((line = br.readLine()) != null);
-        String[] arr=line.split("[,; .\"]+");
-          
+    public static LinkedList2<Integer, String> createIndexList() throws Exception {
 
-        return arr ;
-       // return new LinkedList2<>();
-    } 
+        LinkedList2<Integer, String> indexList = new LinkedList2<Integer, String>();
+        @SuppressWarnings("unchecked")
+        LinkedList<String>[] stop = new LinkedList[26];
+        BufferedReader br = new BufferedReader(new FileReader("resources\\dataset.csv")); // relative path for CVS file
+        BufferedReader br2 = new BufferedReader(new FileReader("resources\\stop.txt")); // file that contains stop words
+        try {
+            String stopword;
+            String first = br2.readLine();
+            for (int i = 0; i < 26; i++) {
+                stop[i] = new LinkedList<String>();
+                stop[i].insert(first);
+                while (true) {
+                    stopword = br2.readLine();
+                    if (stopword.length() == 1) {
+                        first = stopword;
+                        break;
+                    }
+                    stop[i].insert(stopword);
 
-    public static void removeStopWords(LinkedList2<Integer,String> indexList){
+                }
+            }
 
+        } catch (Exception e) {
+            br2.close(); // TODO: handle exception
+        }
+
+        String line = br.readLine(); // to Get rid of the first line in CSV fle
+        String[] words;
+        int index;
+        boolean flag;
+        for (int i = 0; i < 50; i++) {
+
+            line = br.readLine();
+            line = line.toLowerCase();
+            // line= line.replace("'","");
+            words = line.split("[,; .\"" + i + "]+");
+            indexList.insert(i);
+
+            for (String word : words) {
+                flag = true;
+                if (word != "") {
+                    index = word.charAt(0) - 97;
+                    stop[index].findfirst();
+                    while (!stop[index].last()) {
+                        if (word.equals(stop[index].retrieve())) {
+                            flag = false;
+                            break;
+                        }
+                        stop[index].findnext();
+                    }
+                    if (word.equals(stop[index].retrieve()))
+                        flag = false;
+                    if (flag) {
+                        word = word.replace("'", "");
+                        indexList.getList().insert(word);
+                    }
+
+                }
+            }
+        }
+        br.close();
+        return indexList;
     }
 
-    public static LinkedList2<String, Integer> createInvertedIndexList (){
+    // public static void removeStopWords(LinkedList2<Integer, String> indexList) {}
+
+    public static LinkedList2<String, Integer> createInvertedIndexList() {
         return new LinkedList2<>();
     }
 }
